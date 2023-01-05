e4e55b7b9aeccd77baadbe0140285a5222c81e48
suap-dev <suap.dev@gmail.com>
2023-01-05 17:14:22 +0100

    Testing

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,25 @@ use tetronominoes::Tetronomino;
 
 fn main() {
     let mut eng: MyEngine;
-    match MyEngine::init(30, 20, 15) {
+    match MyEngine::init(30, 20, 2) {
        Ok(my_engine) => eng = my_engine,
        Err(e) => panic!("We're having problems initialising the engine: {:?}", e)
     }
 
-    let px: Pixel = Pixel::default();
-    let tet: Tetronomino = Tetronomino::new();
+    // let px: Pixel = Pixel::default();
+    let mut tet: Tetronomino = Tetronomino::new();
     // let mut shape: Vec<Pixel> = Vec::new();
     // shape.push(px);
     // let shape: Shape = Shape::from(shape);
 
-    for _i in 0..45 {
-        eng.draw(&px);
+    for _i in 0..10 {
+        // tet.position.1 += 1;
+
+        tet.rot_left();
+
+        eng.clear();
+
+        // eng.draw(&px);
         eng.draw(&tet);
 
         eng.update_frame();
